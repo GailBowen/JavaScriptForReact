3ca7bb9f720cf1b31835ec1cd8e143f68cd5b2e4
Gail Foad <gail.foad@thenavpeople.co.uk>
2023-01-30 18:00:34 +0000

    Promises

--- promises.js
+++ promises.js
@@ -36,5 +36,15 @@ function createPost(post){
     });
 }
 
-createPost({ title: 'Post The Fourth', body: 'I am the fourth one'})
-.then(getPosts);
+// createPost({ title: 'Post The Fourth', body: 'I am the fourth one'})
+// .then(getPosts)
+// .catch(err => console.log(err));
+
+const promise1 = Promise.resolve('hello world');
+const promise2 = 10;
+const promise3 = new Promise((resolve, reject) =>
+    setTimeout(resolve, 2000, 'Goodbye'));
+
+const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
+
+Promise.all([promise1, promise2, promise3, promise4]).then((values => console.log(values)));
