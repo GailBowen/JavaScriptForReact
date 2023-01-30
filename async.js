afe9175ca40cb211150871d3b3f953568b78310c
Gail Foad <gail.foad@thenavpeople.co.uk>
2023-01-30 18:24:11 +0000

    More fetch

---
+++ async.js
@@ -0,0 +1,25 @@
+let a = 1
+let b = 2
+
+setTimeout(function() { console.log('Timeout: ' + a)}, 100);
+
+// fetch('/').then(function() {
+//     console.log('Fetch')
+// })
+
+a = 99;
+
+async function fetchUsers(){
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+   
+    const data = await res.json();
+   
+    console.log('fetch data', data);
+}
+   
+fetchUsers();
+
+console.log('synchronous code')
+
+console.log(a)
+console.log(b)
