7f5f695688ea05890334784bb4a8ec6bd4cabe15
Gail Foad <gail.foad@thenavpeople.co.uk>
2023-01-30 18:07:19 +0000

    fetch

--- promises.js
+++ promises.js
@@ -40,11 +40,32 @@ function createPost(post){
 // .then(getPosts)
 // .catch(err => console.log(err));
 
-const promise1 = Promise.resolve('hello world');
-const promise2 = 10;
-const promise3 = new Promise((resolve, reject) =>
-    setTimeout(resolve, 2000, 'Goodbye'));
+//async/await
+// async function init(){
+//     await createPost({ title: 'Post The Fourth', body: 'I am the fourth one'});
 
-const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
+//     getPosts();
+// }
 
-Promise.all([promise1, promise2, promise3, promise4]).then((values => console.log(values)));
+// init();
+
+
+//async/await with fetch
+async function fetchUsers(){
+ const res = await fetch('https://jsonplaceholder.typicode.com/users');
+
+ const data = await res.json();
+
+ console.log(data);
+}
+
+fetchUsers();
+
+// const promise1 = Promise.resolve('hello world');
+// const promise2 = 10;
+// const promise3 = new Promise((resolve, reject) =>
+//     setTimeout(resolve, 2000, 'Goodbye'));
+
+// const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
+
+// Promise.all([promise1, promise2, promise3, promise4]).then((values => console.log(values)));
