f7ce4dffde404641908d8b3540e079b2d37312e2
Gail Foad <gail.foad@thenavpeople.co.uk>
2023-02-06 18:21:08 +0000

    Destructuring arrays

--- pedro.js
+++ pedro.js
@@ -48,6 +48,21 @@ const noHarry = newNames.filter((item)=> {
 console.log('🐈 ~ noHarry', noHarry)
 
 
+let a, b, rest;
+[a, b] = [10, 20];
+
+console.log(a);
+// Expected output: 10
+
+console.log(b);
+// Expected output: 20
+
+[a, b, ...rest] = [10, 20, 30, 40, 50];
+
+console.log(rest);
+// Expected output: Array [30, 40, 50]
+
+
 
 
 
